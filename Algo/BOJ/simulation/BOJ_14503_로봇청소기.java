8f69a7ff211ffc57022fda0db85520081b857d5a
1ComputerMaster <87481266+1ComputerMaster@users.noreply.github.com>
2021-12-28 15:05:04 +0900

    commit

--- Algo/BOJ/simulation/BOJ_14503_로봇청소기.java
+++ Algo/BOJ/simulation/BOJ_14503_로봇청소기.java
@@ -3,8 +3,6 @@ package simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_14503_로봇청소기 {
@@ -36,32 +34,31 @@ public class BOJ_14503_로봇청소기 {
 
 	private static void clean(int r, int c, int d) {
 		if(arr[r][c] == 0) {
-			arr[r][c] = 2; //아하 여기서 초기화 하자
+			arr[r][c] = 2;
 			ans++;
 		}
-		boolean check = false;
 		int origin = d;
+		boolean check = true;
 		for (int i = 0; i < 4; i++) {
-			int nd = (d + 3) % 4;
-			int nx = r + dx[nd];
-			int ny = c + dy[nd];
-			if (nx > 0 && ny > 0 && nx < N && ny < M) {
-				if (arr[nx][ny] == 0) { //처음 닫는 공간
-					clean(nx, ny, nd); // 다시 치우고
-					check = true; //여기가 true가 안 된다면 밑에선 한 번도 못간 공간이 된다.
+			d = (d+3)%4;
+			int nx = r + dx[d];
+			int ny = c + dy[d];
+			if(nx >= 0 && ny >= 0 && nx < N && ny < M)
+			{
+				if(arr[nx][ny] == 0)
+				{
+					clean(nx,ny,d);
+					check = false;
 					break;
 				}
 			}
-			d = (d + 3) % 4;
 		}
-		if (!check) { // 4 방향 모두 청소가 되었거나 또는 벽인 경우이다
+		if(check) {
 			int temp = (origin + 2)%4;
 			int nx = r + dx[temp];
 			int ny = c + dy[temp];
-			if (nx > 0 && ny > 0 && nx < N && ny < M) {
-				if (arr[nx][ny] != 1) { //뒤가 벽이 아니면...
-					clean(nx, ny, origin); // 바라보는 방향 유지한 채 후진
-				}
+			if(arr[nx][ny] != 1) {
+				clean(nx,ny,origin);
 			}
 		}
 	}
