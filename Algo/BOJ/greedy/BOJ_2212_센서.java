5cb40dc57f9fca329df4a01b699c1fa3233268d7
1ComputerMaster <87481266+1ComputerMaster@users.noreply.github.com>
2022-01-05 11:08:16 +0900

    s

--- Algo/BOJ/greedy/BOJ_2212_센서.java
+++ Algo/BOJ/greedy/BOJ_2212_센서.java
@@ -1,13 +1,9 @@
 package greedy;
 
-import java.awt.BufferCapabilities;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class BOJ_2212_센서 {
