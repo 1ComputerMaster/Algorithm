fa5564be711a373709c4517e1b3f24b2db6e3700
1ComputerMaster <87481266+1ComputerMaster@users.noreply.github.com>
2022-01-11 16:33:41 +0900

    baseball

--- Algo/BOJ/simulation/BOJ_17281_야구.java
+++ Algo/BOJ/simulation/BOJ_17281_야구.java
@@ -1,12 +1,3 @@
-# 1. 삼성 기출 백준 17281 야구문제
-
-![image](https://user-images.githubusercontent.com/87481266/148859820-ed935291-2ab0-4ec9-a127-41a3cbe4acab.png)
-
-- 만일 모든 주자가 다 뛰었음에도 그대로 이닝이 종료 되지 않았다면 그 때 이닝의 배열 그대로 쓴다. (startPlayer == 9 까지 가면 startPlayer는 다시 1로 들어가야 하는 상황)
-
-
-
-```
 package simulation;
 
 
@@ -164,6 +155,3 @@ public class BOJ_17281_야구 {
 		
 	}//play
 }
-
-
-```
