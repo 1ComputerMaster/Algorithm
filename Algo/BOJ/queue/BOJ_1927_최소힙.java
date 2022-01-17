a8721230f0eeb49073c28519880f6b2cd2552c29
origin <ksd5653@naver.com>
2022-01-17 22:00:00 +0900

    최소 힙

---
+++ Algo/BOJ/queue/BOJ_1927_최소힙.java
@@ -0,0 +1,27 @@
+package queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class BOJ_1927_최소힙 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x));
+		for (int i = 0; i < N; i++) {
+			int v = Integer.parseInt(in.readLine());
+			if(v == 0) {
+				if(pq.isEmpty()) System.out.println(0);
+				else {
+					System.out.println(pq.poll());
+				}
+			}else {
+				pq.add(v);
+			}
+		}
+	}
+}
