19a3cce679f46f5712176870cf3c41e0453d6048
1ComputerMaster <87481266+1ComputerMaster@users.noreply.github.com>
2022-01-19 20:47:58 +0900

    완전 세제곱

---
+++ Algo/BOJ/simulation/BOJ_4690_완전_세제곱.java
@@ -0,0 +1,19 @@
+package simulation;
+
+public class BOJ_4690_완전_세제곱 {
+
+	public static void main(String[] args) {
+		for (int i = 6; i <= 100; i++) {
+			for (int j = 2; j < i; j++) {
+				for (int j2 = j+1; j2 < i; j2++) {
+					for (int k = j2+1; k < i; k++) {
+						if(i*i*i == j*j*j + j2*j2*j2 + k*k*k) {
+							System.out.println("Cube = "+i+", Triple = "+"("+j+","+j2+","+k+")");
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
