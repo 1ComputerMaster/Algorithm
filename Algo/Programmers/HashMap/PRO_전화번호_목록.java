6918d97c4b982fa121746b8c192851cff9bc25a2
1ComputerMaster <87481266+1ComputerMaster@users.noreply.github.com>
2022-03-22 09:08:29 +0900

    PROGRAMMERS

---
+++ Algo/Programmers/HashMap/PRO_전화번호_목록.java
@@ -0,0 +1,18 @@
+package HashMap;
+
+import java.util.*;
+
+public class PRO_전화번호_목록 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Map<String, Integer> map = new HashMap<>(); // 2. 모든 전화번호를 HashMap에 넣는다.
+        for (int i = 0; i < phone_book.length; i++) 
+            map.put(phone_book[i], i); 
+        // 3. 모든 전화번호의 substring이 HashMap에 존재하는지 확인한다. 
+        for (int i = 0; i < phone_book.length; i++) 
+            for (int j = 0; j < phone_book[i].length(); j++) 
+                if (map.containsKey(phone_book[i].substring(0, j))) 
+                    return false;
+        return answer;
+    }
+}
