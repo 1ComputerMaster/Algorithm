bc067a6db4244ffd95efbc578873ca4ff8d9f468
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2022-04-15 16:41:40 +0900

    Programmers

---
+++ Algo/Programmers/simulation/PRO_수박수박.java
@@ -0,0 +1,15 @@
+package simulation;
+
+public class PRO_수박수박 {
+    public String solution(int n) {
+        String answer = "";
+        for(int i = 0; i < n; i++){
+            if(i%2 == 0){
+                answer += "수";
+            }else{
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
