ef0fdd85360179e02e94f167662b64b79aa718de
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-01-24 21:50:51 +0900

    [FIX]-다익스트라, 구현
    * 이상

---
+++ src/BOJ/Dijkstra/BOJ_11265.java
@@ -0,0 +1,39 @@
+package BOJ.Dijkstra;
+import java.util.Scanner;
+
+public class BOJ_11265 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[][] time = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                time[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (time[i][j] > time[i][k] + time[k][j]) {
+                        time[i][j] = time[i][k] + time[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int m = 0; m < M; m++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            int C = scanner.nextInt();
+            if (time[A][B] <= C) {
+                System.out.println("Enjoy other party");
+            } else {
+                System.out.println("Stay here");
+            }
+        }
+    }
+}
+
