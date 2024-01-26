aa270f701ff7839249bb804cee261524b462111b
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-01-26 20:35:49 +0900

    [FEAT] 출력문제 추가

---
+++ src/BOJ/simulation/BOJ_2377.java
@@ -0,0 +1,17 @@
+package BOJ.simulation;
+
+public class BOJ_2377 {
+    public static void main(String[] args) {
+        System.out.println(" _________\n" +
+                " \\_     _/\n" +
+                "   \\   /\n" +
+                "    | |\n" +
+                "   /   \\\n" +
+                "  /     \\\n" +
+                " |       |\n" +
+                "/---------\\\n" +
+                "| \\-/ \\-/ |\n" +
+                "\\---------/\n" +
+                " \\_______/\n");
+    }
+}
