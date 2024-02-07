1c2930d8a8de877de37c6aeeb6a283e6d743e61a
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-02-07 20:01:20 +0900

    [FEAT] 벨만 포드 알고리즘 구현

--- src/BOJ/shortest_path/BOJ_11657.java
+++ src/BOJ/shortest_path/BOJ_11657.java
@@ -15,8 +15,8 @@ public class BOJ_11657 {
 
         List<Edge> edges = new ArrayList<>();
 
-        int[] minDist = new int[N + 1];
-        Arrays.fill(minDist, Integer.MAX_VALUE);
+        long[] minDist = new long[N + 1];
+        Arrays.fill(minDist, 100_000_000);
 
         for (int i = 0; i < M; i++){
             tk = new StringTokenizer(in.readLine()," ");
@@ -27,51 +27,44 @@ public class BOJ_11657 {
             edges.add(new Edge(from, to, dist));
         }
 
-        if(bellmanFord(edges, minDist, 1)){
-            // 1번 노드를 제외한 나머지 노드로 가는 최단 거리를 출력합니다.
+        if(bellmanFord(edges, minDist)){
             for (int i = 2; i <= N; i++) {
-                // 도달할 수 없는 경우 -1을 출력합니다.
-                if(minDist[i] == Integer.MAX_VALUE){
+                if(minDist[i] == 100_000_000){
                     System.out.println("-1");
                 }
-                // 도달할 수 있는 경우 거리를 출력합니다.
                 else {
                     System.out.println(minDist[i]);
                 }
             }
         }
-        // 음수 사이클이 존재하는 경우 -1을 출력합니다.
         else {
             System.out.println("-1");
         }
-
     }
 
-    private static boolean bellmanFord(List<Edge> edges, int[] minDist, int start) {
-        minDist[start] = 0;
-        // (노드의 개수 - 1)번 만큼 반복합니다.
-        for (int i = 0; i < N - 1; i++) {
-            // 모든 간선을 확인합니다.
-            for (Edge edge : edges) {
-                if (minDist[edge.from] != Integer.MAX_VALUE && minDist[edge.to] > minDist[edge.from] + edge.dist) {
+    private static boolean bellmanFord(List<Edge> edges, long[] minDist) {
+        boolean check = true;
+        minDist[1] = 0;
+        for (int i = 0; i < N - 1; i++){
+            for (Edge edge : edges){
+                if(minDist[edge.from] != 100_000_000 && minDist[edge.to] > minDist[edge.from] + edge.dist){
                     minDist[edge.to] = minDist[edge.from] + edge.dist;
                 }
             }
         }
-        for (Edge edge : edges) {
-            // 최단 거리 테이블이 갱신되는 경우가 있으면 음수 사이클이 존재합니다.
-            if (minDist[edge.from] != Integer.MAX_VALUE && minDist[edge.to] > minDist[edge.from] + edge.dist) {
-                return false;
+        for (Edge edge : edges){
+            if (minDist[edge.from] != 100_000_000 && minDist[edge.to] > minDist[edge.from] + edge.dist){
+                check = false;
             }
         }
-        // 음수 사이클이 존재하지 않으면 true를 반환합니다.
-        return true;
+        return check;
     }
 
-    static class Edge {
-        int from, to, dist;
-
-        public Edge(int from, int to, int dist) {
+    static class Edge{
+        int from;
+        int to;
+        int dist;
+        public Edge(int from, int to, int dist){
             this.from = from;
             this.to = to;
             this.dist = dist;
