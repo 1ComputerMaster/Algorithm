ac1ca851084a182f7c5db190f188fbfa5506a681
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-03-13 21:56:34 +0900

    [FEAT] Simulation

---
+++ src/BOJ/simulation/BOJ_25311.java
@@ -0,0 +1,11 @@
+package BOJ.simulation;
+
+import java.util.Scanner;
+
+public class BOJ_25311 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println("A");
+    }
+}
