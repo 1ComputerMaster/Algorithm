c15591ef6fec8608fa6dd2730a360d73c962668a
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-03-19 21:58:26 +0900

    [FEAT] Simulation

---
+++ src/BOJ/simulation/BOJ_10189.java
@@ -0,0 +1,10 @@
+package BOJ.simulation;
+
+public class BOJ_10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #\n");
+    }
+}
