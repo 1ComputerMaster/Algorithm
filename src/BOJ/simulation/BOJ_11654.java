6b2cb6c6aae7940157b23a4696729d4bdea0e26b
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-03-21 23:46:06 +0900

    [FEAT] DynamicProgramming

---
+++ src/BOJ/simulation/BOJ_11654.java
@@ -0,0 +1,13 @@
+package BOJ.simulation;
+
+import java.util.Scanner;
+
+public class BOJ_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char n = sc.next().toCharArray()[0];
+
+        System.out.println((int) n);
+    }
+}
