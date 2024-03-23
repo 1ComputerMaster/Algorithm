c1e2833c84c735f87aea98515efe6de997abb501
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-03-23 17:46:45 +0900

    [FEAT] DynamicProgramming

---
+++ src/BOJ/simulation/BOJ_14337.java
@@ -0,0 +1,13 @@
+package BOJ.simulation;
+
+public class BOJ_14337 {
+    public static void main(String[] args) {
+        System.out.println("      ===================\n" +
+                "          ____||___\n" +
+                "\\ /      /       []\\\n" +
+                " X=======           \\__\n" +
+                "/ \\      \\____________|\n" +
+                "            ||  ||\n" +
+                "         \\-----------/\n");
+    }
+}
