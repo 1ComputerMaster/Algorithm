c93e2d194fa4e3bc7ee6a1c6775842731e3f658f
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-05-25 21:36:42 +0900

    [FEAT] String

--- src/BOJ/String/BOJ_16916.java
+++ src/BOJ/String/BOJ_16916.java
@@ -1,18 +1,67 @@
 package BOJ.String;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class BOJ_16916 {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String S = in.readLine();
         String P = in.readLine();
-        if(S.contains(P)){
-            System.out.println("1");
-        }else{
-            System.out.println("0");
+
+        if (kmpSearch(S, P)) {
+            bw.write("1");
+        } else {
+            bw.write("0");
         }
+        bw.flush();
+        bw.close();
+    }
+
+    public static boolean kmpSearch(String text, String pattern) {
+        int[] lps = computeLPSArray(pattern);
+        int i = 0;  // index for text
+        int j = 0;  // index for pattern
+
+        while (i < text.length()) {
+            if (pattern.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == pattern.length()) {
+                return true;  // found pattern at index i - j
+            } else if (i < text.length() && pattern.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int[] computeLPSArray(String pattern) {
+        int length = 0;  // length of the previous longest prefix suffix
+        int i = 1;
+        int[] lps = new int[pattern.length()];
+        lps[0] = 0;  // lps[0] is always 0
+
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(length)) {
+                length++;
+                lps[i] = length;
+                i++;
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
 }
