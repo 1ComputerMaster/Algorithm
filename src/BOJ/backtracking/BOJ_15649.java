00f40b3ff1a7b1c26166660773ca8de22840ccfe
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-07-21 15:40:45 +0900

    [FEAT] backtracking

---
+++ src/BOJ/backtracking/BOJ_15649.java
@@ -0,0 +1,47 @@
+package BOJ.backtracking;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_15649 {
+    static int N,M;
+    static StringBuilder sb;
+    static int[] arr;
+    public static void main(String[] args) throws Exception{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(in.readLine()," ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(tk.nextToken());
+        M = Integer.parseInt(tk.nextToken());
+        int visted[] = new int[N];
+
+        arr = new int[M];
+        backtracking(visted, 0);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void backtracking(int[] visted, int cnt) {
+        if(cnt == M){
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++){
+            if(visted[i] == 0){
+                visted[i] = 1;
+                arr[cnt] = i + 1;
+                backtracking(visted, cnt + 1);
+                visted[i] = 0;
+            }
+        }
+    }
+}
