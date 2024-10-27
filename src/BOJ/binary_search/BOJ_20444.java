112ec7f5cc8d03019812dbf6b11d6361777d1f46
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-10-27 20:42:16 +0900

    [FEAT] Binary Search

--- src/BOJ/binary_search/BOJ_20444.java
+++ src/BOJ/binary_search/BOJ_20444.java
@@ -6,7 +6,6 @@ public class BOJ_20444 {
     /**
      * 최초에는 가로 세로 선택에 따라서 달라지는 줄 알았으나
      * 실제로는 줄이 그이는 것 자체에 따라서 영향을 받기 때문에 이진 탐색으로 쉽게 구할 수 있었습니다.
-     * @param args
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
