cb333e48b45e33c1f3a3fcf6208d13f930ef3637
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-11-03 00:11:02 +0900

    [FIX] Binary Search

---
+++ src/BOJ/binary_search/BOJ_2412.java
@@ -0,0 +1,62 @@
+package BOJ.binary_search;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_2412 {
+    static int N, T;
+    static ArrayList<Integer>[] points;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(in.readLine(), " ");
+        N = Integer.parseInt(tk.nextToken());
+        T = Integer.parseInt(tk.nextToken());
+        points = new ArrayList[20001];
+        for (int i = 0; i < 20001; i++){
+            points[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < N; i++) {
+            tk = new StringTokenizer(in.readLine(), " ");
+            int x = Integer.parseInt(tk.nextToken());
+            int y = Integer.parseInt(tk.nextToken());
+            points[y].add(x);
+        }
+        for (int i = 0; i < 20001; i++)
+            Collections.sort(points[i]);
+
+        int result = bfs();
+        System.out.println(result);
+    }
+
+    private static int bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[] visited = new boolean[20001];
+
+        int steps = 0;
+        queue.add(new int[] {0,0});
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int s = 0; s < size; s++) {
+                int[] cur = queue.poll();
+                int cx = cur[0], cy = cur[1];
+                if (cy == T) return steps; // 정상 도착 시 반환
+                for (int y = cur[1] - 2; y <= cur[1] + 2; y++) {
+                    if (y < 0 || y >= 20001) continue;
+                    for (int j = 0; j < points[y].size(); j++) {
+                        int x = points[y].get(j);
+                        if (cx + 2 < x) break;
+                        else if (cx - 2 > x) continue;
+                        points[y].remove(j);
+                        queue.add(new int[]{x, y});
+                        j--;
+                    }
+                }
+            }
+            steps++;
+        }
+
+        return -1; // 정상 도달 불가 시 -1 반환
+    }
+}
