71bf6af436d98bd374896ef005abcf43891761f9
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2024-12-05 22:30:43 +0900

    [FEAT] Floyd-warshall

--- src/BOJ/shortest_path/BOJ_21940.java
+++ src/BOJ/shortest_path/BOJ_21940.java
@@ -10,21 +10,21 @@ public class BOJ_21940 {
         int M = sc.nextInt();
 
         long[][] dist = new long[N + 1][N + 1];
-
-        // 거리 배열 초기화
         for (int i = 1; i <= N; i++) {
             Arrays.fill(dist[i], Long.MAX_VALUE / 2);
             dist[i][i] = 0;
         }
-
-        // 간선 정보 입력
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < M; i++){
             int from = sc.nextInt();
             int to = sc.nextInt();
-            int weight = sc.nextInt();
-            dist[from][to] = Math.min(dist[from][to], weight);
+            long weight = sc.nextLong();
+            dist[from][to] = weight;
         }
-
+        int K = sc.nextInt();
+        List<Integer> friends = new ArrayList<>();
+        for (int k = 0; k < K; k++){
+            friends.add(sc.nextInt());
+        }
         // 플로이드-워셜 알고리즘
         for (int k = 1; k <= N; k++) {
             for (int i = 1; i <= N; i++) {
@@ -33,42 +33,26 @@ public class BOJ_21940 {
                 }
             }
         }
-
-        // 친구들의 위치 입력
-        int K = sc.nextInt();
-        int[] friends = new int[K];
-        for (int i = 0; i < K; i++) {
-            friends[i] = sc.nextInt();
-        }
-
-        // 최소 왕복 거리 계산
-        long minMaxDistance = Long.MAX_VALUE;
-        List<Integer> ansCities = new ArrayList<>();
-
-        for (int city = 1; city <= N; city++) {
+        long minDist = Long.MAX_VALUE / 2;
+        List<Integer> ansList = new ArrayList<>();
+        for (int go = 1; go <= N; go++) {
             long maxDistance = 0;
-
             for (int friend : friends) {
-                //단뱡항 그래프
-                if (dist[friend][city] == Long.MAX_VALUE/2 || dist[city][friend] == Long.MAX_VALUE /2) {
-                    maxDistance = Long.MAX_VALUE;
-                    break;
+                if (dist[friend][go] != Long.MAX_VALUE / 2 && dist[go][friend] != Long.MAX_VALUE / 2) {
+                    maxDistance = Math.max(maxDistance, dist[friend][go] + dist[go][friend]);
                 }
-                //왕복 시간이 다름
-                maxDistance = Math.max(maxDistance, dist[friend][city] + dist[city][friend]);
             }
-
-            // 최소 왕복 거리 비교 및 갱신
-            if (maxDistance < minMaxDistance) {
-                minMaxDistance = maxDistance;
-                ansCities.clear();
-                ansCities.add(city);
-            } else if (maxDistance == minMaxDistance) {
-                ansCities.add(city);
+            if(minDist > maxDistance){
+                minDist = maxDistance;
+                ansList.clear();
+                ansList.add(go);
+            }
+            else if(minDist == maxDistance){
+                ansList.add(go);
             }
         }
-        Collections.sort(ansCities);
-        for (int city : ansCities)
-            System.out.print(city + " ");
+        for (int ans : ansList){
+            System.out.print(ans + " ");
+        }
     }
 }
