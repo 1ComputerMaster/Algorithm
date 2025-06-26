eecadeca3b66e6d7f18ecea4d6c8cfb388c045b4
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-06-26 21:01:38 +0900

    [ADD] LeetCode - WordBreak

--- src/LeetCode/WordBreak.java
+++ src/LeetCode/WordBreak.java
@@ -9,9 +9,9 @@ public class WordBreak {
     public static boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
-        for (int i = 1; i < s.length(); i++){
+        for (int i = 1; i <= s.length(); i++){
             for (String word : wordDict) {
-                if (i - word.length() >= 0
+                if (i >= word.length()
                     && dp[i - word.length()]
                     && s.startsWith(word, i - word.length())){
                     dp[i] = true;
