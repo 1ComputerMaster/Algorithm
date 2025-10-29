92049d4ba05721745ee2fe73f2b80cadd98c866e
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-10-29 19:28:59 +0900

    [ADD] LeetCode - Permutations retry

--- src/LeetCode/Permutations.java
+++ src/LeetCode/Permutations.java
@@ -4,24 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
-    static int n = 0;
-    static List<List<Integer>> ansList;
-    public List<List<Integer>> permute(int[] nums) {
-        n = nums.length;
-        ansList = new ArrayList<>();
-        perm(nums, new ArrayList<>(), new boolean[n]);
-        return ansList;
+    
+    List<List<Integer>> ans;
+
+    public static void main(String[] args) {
+        Permutations permutations = new Permutations();
+
+        System.out.println(permutations.permute(new int[] {1,2,3}));
+
     }
-    private void perm(int[] arr, List<Integer> num, boolean[] visited){
-        if(n == num.size()){
-            ansList.add(new ArrayList<>(num));
+    public List<List<Integer>> permute(int[] nums) {
+        ans = new ArrayList<>();
+        perm(new boolean[nums.length], nums, 0, new ArrayList());
+        return ans;
+    }
+    private void perm(boolean[] visited, int[] nums, int cnt, List<Integer> cur){
+        if(cnt == nums.length){
+            ans.add(new ArrayList(cur));
+            return;
         }
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < nums.length; i++){
             if(!visited[i]){
                 visited[i] = true;
-                num.add(arr[i]);
-                perm(arr, num, visited);
-                num.remove(num.size() - 1);
+                cur.add(nums[i]);
+                perm(visited, nums, cnt + 1, cur);
+                cur.remove(cur.size() - 1);
                 visited[i] = false;
             }
         }
