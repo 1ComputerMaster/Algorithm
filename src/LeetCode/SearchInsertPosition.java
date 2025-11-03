783c108da8e289a7b7a9c4cb6ab60d76f8234393
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-11-03 19:40:36 +0900

    [ADD] LeetCode - SearchInsertPosition 재풀이

--- src/LeetCode/SearchInsertPosition.java
+++ src/LeetCode/SearchInsertPosition.java
@@ -2,24 +2,28 @@ package LeetCode;
 
 public class SearchInsertPosition {
     public static void main(String[] args) {
+        SearchInsertPosition sol = new SearchInsertPosition();
         int[] nums = {1,3,5,6};
         int target = 7;
-        System.out.println(searchInsert(nums, target));
+        System.out.println(sol.searchInsert(nums, target));
     }
-    public static int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return mid;        // 찾았으면 그 인덱스 반환
-            } else if (nums[mid] < target) {
-                left = mid + 1;    // target 이 더 크면 오른쪽 절반 탐색
-            } else {
-                right = mid - 1;   // target 이 더 작으면 왼쪽 절반 탐색
+        while(left <= right){
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            }else{
+                if(nums[mid] > target){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
             }
         }
         return left;
     }
-
 }
