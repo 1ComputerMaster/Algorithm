7fbdb1acfe870d5272cb12d69d974816213b37d6
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-11-09 10:24:27 +0900

    [ADD] LeetCode - UniquePaths

---
+++ src/LeetCode/UniquePaths.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class UniquePaths {
+    public static void main(String[] args) {
+        UniquePaths sol = new UniquePaths();
+        int m = 3;
+        int n = 7;
+        int ans = sol.uniquePaths(m, n);
+        System.out.println(ans);
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
