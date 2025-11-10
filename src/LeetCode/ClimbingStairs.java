abcdfd186b7c59e8b00e3d37bae5fa0662b20680
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-11-10 20:53:59 +0900

    [ADD] LeetCode - ClimbingStairs 재풀이

--- src/LeetCode/ClimbingStairs.java
+++ src/LeetCode/ClimbingStairs.java
@@ -5,20 +5,10 @@ public class ClimbingStairs {
         System.out.println(climbStairs(5));
     }
     public static int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        if(n == 1){
-            return 1;
-        }
-        if(n == 2){
-            return 2;
-        }
-        if(n == 3){
-            return 3;
-        }
+        int[] dp = new int[46];
         dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        for(int i = 3; i <= n; i++){
+        dp[0] = 1;
+        for(int i = 2; i < 46; i++){
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
