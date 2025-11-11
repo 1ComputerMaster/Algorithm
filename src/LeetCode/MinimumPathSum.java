ff76809a0407a1061cf13ca56b3f0861c047e2d6
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-11-11 20:19:24 +0900

    [ADD] LeetCode - MinimumPathSum 재풀이

--- src/LeetCode/MinimumPathSum.java
+++ src/LeetCode/MinimumPathSum.java
@@ -11,24 +11,27 @@ public class MinimumPathSum {
         System.out.println(m.minPathSum(grid)); // Should return 7
     }
     public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        int[][] dp = new int[n + 1][m + 1];
-
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(i - 1 != 0 && j - 1 != 0){
-                    dp[i][j] = Math.min(grid[i - 1][j - 1] + dp[i - 1][j], grid[i - 1][j - 1] + dp[i][j - 1]);
-                }
-                if(i - 1 == 0){
-                    dp[i][j] = grid[i - 1][j - 1] + dp[i][j - 1];
-                }
-                if(j - 1 == 0){
-                    dp[i][j] = grid[i - 1][j - 1] + dp[i - 1][j];
-                }
+        int[][] dp = new int[grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                dp[i][j] = grid[i][j];
             }
         }
-        return dp[n][m];
+
+        for(int i = 1; i < grid.length; i++){
+            dp[i][0] += dp[i - 1][0];
+        }
+
+        for(int j = 1; j < grid[0].length; j++){
+            dp[0][j] += dp[0][j - 1];
+        }
+
+
+        for(int i = 1; i < grid.length; i++){
+            for(int j = 1; j < grid[0].length; j++){
+                dp[i][j] += Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
     }
 }
