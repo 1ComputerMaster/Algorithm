605be0b9d14f1a099db816df609bc1a221315495
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-11-14 23:42:11 +0900

    [ADD] LeetCode - WordBreak 재풀이

--- src/LeetCode/WordBreak.java
+++ src/LeetCode/WordBreak.java
@@ -9,11 +9,10 @@ public class WordBreak {
     public static boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
-        for (int i = 1; i <= s.length(); i++){
-            for (String word : wordDict) {
-                if (i >= word.length()
-                    && dp[i - word.length()]
-                    && s.startsWith(word, i - word.length())){
+        for(int i = 1; i <= s.length(); i++){
+            for(String w : wordDict){
+                int start = i - w.length();
+                if(start >= 0 && dp[start] && s.substring(start, i).equals(w)){
                     dp[i] = true;
                     break;
                 }
