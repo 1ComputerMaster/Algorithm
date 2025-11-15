a4a7abf287b6ef746c7e1d9deff93452d39b4666
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-11-15 12:22:20 +0900

    [ADD] LeetCode - HouseRobber

--- src/LeetCode/HouseRobber.java
+++ src/LeetCode/HouseRobber.java
@@ -2,26 +2,29 @@ package LeetCode;
 
 public class HouseRobber {
     public static void main(String[] args) {
-        System.out.println(rob(new int[]{1, 2, 3, 1})); // Example usage
+        HouseRobber sol = new HouseRobber();
+        System.out.println(sol.rob(new int[]{1, 2, 3, 1})); // Example usage
     }
-    public static int rob(int[] nums) {
+    public int rob(int[] nums) {
         if(nums.length == 1){
             return nums[0];
         }
-        if(nums.length == 2){
-            return Math.max(nums[0], nums[1]);
-        }
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        dp[1] = nums[1];
-        for(int i = 0; i < nums.length - 2; i++){
-            if(i + 3 < nums.length){
-                dp[i + 2] = Math.max(dp[i] + nums[i + 2], dp[i + 2]);
-                dp[i + 3] = Math.max(dp[i] + nums[i + 3], dp[i + 3]);
-            }else{
-                dp[i + 2] = Math.max(dp[i] + nums[i + 2], dp[i + 2]);
-            }
+        int max = 0;
+
+        if(nums.length > 2){
+            dp[1] = nums[1];
+            dp[2] = Math.max(nums[2], dp[0] + nums[2]);
+            max = Math.max(max, Math.max(dp[1], dp[2]));
+        }else{
+            return Math.max(nums[0], nums[1]);
         }
-        return Math.max(dp[nums.length - 1], dp[nums.length - 2]);
+        for(int i = 3; i < nums.length; i++)
+        {
+            dp[i] = Math.max(dp[i - 3] + nums[i], dp[i - 2] + nums[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
     }
 }
