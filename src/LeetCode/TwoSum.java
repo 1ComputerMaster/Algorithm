c53a0bd093ac3ca06d55f892ae916f7fe3280a1d
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-11-16 20:30:44 +0900

    [ADD] LeetCode - TwoSum - 재풀이

--- src/LeetCode/TwoSum.java
+++ src/LeetCode/TwoSum.java
@@ -1,19 +1,25 @@
 package LeetCode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{2,7,1,1,0,5}, 9)));
     }
     public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
+            map.put(nums[i],i);
+        }
+        int cnt = 0;
+        for(int i = 0; i < nums.length; i++){
+            cnt = map.getOrDefault(target - nums[i], -1);
+            if(cnt != -1 && i != cnt){
+                return new int[] {i, cnt};
             }
         }
-        return null;
+        return new int[]{};
     }
 }
