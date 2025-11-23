c4db362f94350973a08b564c2ddeac73c29edcca
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2025-11-23 12:23:57 +0900

    [ADD] LeetCode - NumberOfIslands - 재풀이

--- src/LeetCode/NumberOfIslands.java
+++ src/LeetCode/NumberOfIslands.java
@@ -19,38 +19,40 @@ public class NumberOfIslands {
 
     }
 
+    static boolean[][] visited;
     public static int numIslands(char[][] grid) {
         n = grid.length;
         m = grid[0].length;
-        boolean[][] visited = new boolean[n][m];
+        visited = new boolean[n][m];
         int cnt = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(!visited[i][j] && grid[i][j] == '1')
-                {
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    bfs(grid, i, j);
                     cnt++;
-                    BFS(i, j, visited, grid);
                 }
             }
         }
         return cnt;
     }
-
-    private static void BFS(int startX, int startY, boolean[][] visited, char[][] grid){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{startX, startY});
+    private static void bfs(char[][] grid, int x, int y){
+        Queue<int[]> q = new LinkedList();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
+            int cx = cur[0];
+            int cy = cur[1];
             for(int d = 0; d < 4; d++){
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-                if(nx < n && ny < m && ny >= 0 && nx >= 0 && !visited[nx][ny] && grid[nx][ny] == '1'){
-                    q.add(new int[]{nx,ny});
-                    visited[nx][ny] = true;
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || grid[nx][ny] != '1'){
+                    continue;
                 }
+                visited[nx][ny] = true;
+                q.add(new int[]{nx, ny});
             }
         }
+        return;
     }
 }
