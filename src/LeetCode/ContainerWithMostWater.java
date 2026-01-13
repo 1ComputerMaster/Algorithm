661785e49bda340f3cb017d8c826f69ffe56f7e4
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2026-01-13 19:37:30 +0900

    [ADD] LeetCode - ContainerWithMostWater - retry

--- src/LeetCode/ContainerWithMostWater.java
+++ src/LeetCode/ContainerWithMostWater.java
@@ -9,16 +9,15 @@ public class ContainerWithMostWater {
     public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int max = 0;
-        while(left <= right){
-            int minHeight = Math.min(height[left], height[right]);
-            max = Math.max((minHeight * (right - left)), max);
-            if(height[left] < height[right]){
-                left++;
-            }else{
+        int maxArea = 0;
+        while(left < right){
+            maxArea = Math.max(maxArea, (Math.min(height[left], height[right]) * (right - left)));
+            if(height[left] > height[right]){
                 right--;
+            }else{
+                left++;
             }
         }
-        return max;
+        return maxArea;
     }
 }
