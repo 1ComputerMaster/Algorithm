ad74cbcc975db4a5607ce6906500baf7233807f4
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2026-03-16 19:27:40 +0900

    [FIX] LeetCode - RemoveNthNodeFromEndOfList

--- src/LeetCode/RemoveNthNodeFromEndOfList.java
+++ src/LeetCode/RemoveNthNodeFromEndOfList.java
@@ -4,9 +4,18 @@ public class RemoveNthNodeFromEndOfList {
     static class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
     public static void main(String[] args) {
@@ -21,21 +30,25 @@ public class RemoveNthNodeFromEndOfList {
             result = result.next;
         }
     }
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
         ListNode dummy = new ListNode(0, head);
-        ListNode fast = dummy, slow = dummy;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
 
-        // fast를 n칸 미리 전진
-        for (int i = 0; i < n; i++) fast = fast.next;
-
-        // fast가 끝에 도달할 때 slow는 삭제할 노드의 직전
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
         while (fast.next != null) {
             fast = fast.next;
             slow = slow.next;
         }
 
-        slow.next = slow.next.next;  // 삭제
-        return dummy.next;           // head 삭제 케이스도 일관 처리
-    }
+        slow.next = slow.next.next;
 
+        return dummy.next;
+    }
 }
