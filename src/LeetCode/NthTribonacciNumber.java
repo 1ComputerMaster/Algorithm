4be00c85550e681d9d26a00fdf69e9311570b2b3
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2026-03-18 21:50:50 +0900

    [ADD] LeetCode - NthTribonacciNumber

---
+++ src/LeetCode/NthTribonacciNumber.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class NthTribonacciNumber {
+    public static void main(String[] args) {
+        NthTribonacciNumber obj = new NthTribonacciNumber();
+        int n = 25;
+        System.out.println(obj.tribonacci(n)); // Output: 1389537
+    }
+    public int tribonacci(int n) {
+        if(n == 0){
+            return 0;
+        }
+
+        if(n <= 2){
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        return dp[n];
+    }
+}
