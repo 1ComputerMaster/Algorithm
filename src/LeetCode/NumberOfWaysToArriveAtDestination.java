c3dacf511d334aaf118ecbe10b718eed9896fd69
김형우 <87481266+1ComputerMaster@users.noreply.github.com>
2026-08-30 16:38:10 +0900

    [REFINE] Optimize path counting logic and improve variable naming in NumberOfWaysToArriveAtDestination

--- src/LeetCode/NumberOfWaysToArriveAtDestination.java
+++ src/LeetCode/NumberOfWaysToArriveAtDestination.java
@@ -9,8 +9,8 @@ public class NumberOfWaysToArriveAtDestination {
         NumberOfWaysToArriveAtDestination obj = new NumberOfWaysToArriveAtDestination();
         System.out.println(obj.countPaths(n, roads));
     }
+    final long MOD = (long) 1e9 + 7;
     public int countPaths(int n, int[][] roads) {
-        long mod = (long) 1e9 + 7;
 
         long[] dist = new long[n];
         long[] ways = new long[n];
@@ -41,23 +41,25 @@ public class NumberOfWaysToArriveAtDestination {
 
         while (!pq.isEmpty()) {
             long[] curr = pq.poll();
-            int node = (int) curr[0];
-            long time = curr[1];
-            if(time > dist[node]){
-                continue;
-            }
-            for (int[] next : graph[node]){
-                int nextNode = next[0];
-                long nextTime = next[1] + time;
+            int curNode = (int) curr[0];
+            long curTime = curr[1];
 
-                if (nextTime < dist[nextNode]) {
-                    dist[nextNode] = nextTime;
-                    ways[nextNode] = ways[node];
-                    pq.offer(new long[]{nextNode, nextTime});
-                } else if (nextTime == dist[nextNode]) {
-                    ways[nextNode] = (ways[nextNode] + ways[node]) % mod;
+            if (dist[curNode] < curTime) continue;
+
+            for (int[] next : graph[curNode]){
+                int nextNode = next[0];
+                int nextTime = next[1];
+
+                if (dist[nextNode] > curTime + nextTime){
+                    dist[nextNode] = curTime + nextTime;
+                    ways[nextNode] = ways[curNode];
+
+                    pq.offer(new long[]{nextNode, nextTime + curTime});
+                }else if (dist[nextNode] == curTime + nextTime){
+                    ways[nextNode] = (ways[nextNode] + ways[curNode]) % MOD;
                 }
             }
+
         }
         return (int) ways[n - 1];
     }
